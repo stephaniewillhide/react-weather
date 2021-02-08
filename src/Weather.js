25aded424c435d6fd436dd81659e4f05903f4781
Stephanie Willhide <stephaniewillhide@gmail.com>
2021-02-07 20:29:42 -0700

    Secure URL

--- src/Weather.js
+++ src/Weather.js
@@ -22,7 +22,7 @@ export default function Weather(props) {
 
 	function search() {
 		const apiKey = "ed136b54c46892c2f08167afd8eae8a3";
-		let apiUrl = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+		let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 		axios.get(apiUrl).then(handleResponse);		
 
 	}
