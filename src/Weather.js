d774cda295d6b6bc3f19eb35806a2db248e2c640
Stephanie Willhide <stephaniewillhide@gmail.com>
2021-02-07 20:38:42 -0700

    Icon

--- src/Weather.js
+++ src/Weather.js
@@ -15,7 +15,7 @@ export default function Weather(props) {
 			wind: response.data.wind.speed,
 			city: response.data.name,
 			date: new Date(response.data.dt * 1000),
-			iconUrl: "https://ssl.gstatic.com/onebox/weather/64/cloudy.png",
+			iconUrl: `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
 			description: response.data.weather[0].description,
 		});
 	};
