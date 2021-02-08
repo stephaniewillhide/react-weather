71135c78c37f7aefd2f9f0f73a727439efaf18f0
Stephanie Willhide <stephaniewillhide@gmail.com>
2021-02-08 16:00:38 -0700

    Convert to Fahrenheit

--- src/WeatherTemperature.js
+++ src/WeatherTemperature.js
@@ -2,23 +2,39 @@ import React, { useState } from "react";
 
 export default function WeatherTemperature(props) {
   const [unit, setUnit ] = useState('celsius');
-  function convertToFahrenheit(event) {
-
+  function showFahrenheit(event) {
+    event.preventDefault();
+    setUnit("fahrenheit");
   }
+  function showCelsius(event) {
+    event.preventDefault();
+    setUnit("celsius");
+  }
+
+  function fahrenheit() {
+    return (props.celsius * 9) / 5 + 32;
+  }
+  
   if (unit === 'celsius') {
     return (
       <div className="WeatherTemperature d-inline">
-        <h1 className="d-inline">{Math.round(props.celsius)}</h1>
+        <span className="temperature">{Math.round(props.celsius)}</span>
+        <span className="unit">
+          °C |{" "}
+          <a href="/" onClick={showFahrenheit}>
+            °F
+          </a>
+        </span>
       </div>
     );
   } else {
     return (
       <div className="WeatherTemperature d-inline">
-        {/* <span className="temperature">{Math.round(fahrenheit())}</span> */}
+        <span className="temperature">{Math.round(fahrenheit())}</span>
         <span className="unit">
-          {/* <a href="/" onClick={showCelsius}>
+          <a href="/" onClick={showCelsius}>
             °C
-          </a>{" "} */}
+          </a>{" "}
           | °F
         </span>
       </div>
