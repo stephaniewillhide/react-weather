f1a30d074b0ba495de6216b46ea50bd8d6b972f8
Stephanie Willhide <stephaniewillhide@gmail.com>
2021-02-07 19:40:29 -0700

    Create Date & Time Component

--- src/Weather.js
+++ src/Weather.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import FormattedDate from "./FormattedDate"
 import axios from "axios";
 import "./Weather.css";
 
@@ -13,7 +14,7 @@ export default function Weather(props) {
 			humidity: response.data.main.humidity,
 			wind: response.data.wind.speed,
 			city: response.data.name,
-			date: "CHANGE ME",
+			date: new Date(response.data.dt * 1000),
 			iconUrl: "https://ssl.gstatic.com/onebox/weather/64/cloudy.png",
 			description: response.data.weather[0].description,
 		});
@@ -29,7 +30,9 @@ export default function Weather(props) {
 								<div className="col-8">
 									<h1>{weatherData.city}</h1>
 									<ul>
-										<li>{weatherData.date}</li>
+										<li>
+											<FormattedDate date={weatherData.date} />
+										</li>
 										<li className="text-capitalize">{weatherData.description}</li>
 									</ul>
 								</div>
